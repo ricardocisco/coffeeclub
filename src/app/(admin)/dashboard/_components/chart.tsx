8b9eb7d51b48d9bb7164dc0ba2d7002ad5e6fcbe
ricardocisco <ricardopenga70@gmail.com>
2024-11-13 18:00:01 -0300

    update gráfico, update na responsividade do dashboard

--- src/app/(admin)/dashboard/_components/chart.tsx
+++ src/app/(admin)/dashboard/_components/chart.tsx
@@ -17,35 +17,63 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useState } from "react";
 
 const chartConfig = {
   pedidos: {
     label: "Pedidos",
-    color: "#2563eb",
+    color: "hsl(var(--chart-1))",
   },
   usuarios: {
     label: "Usuarios",
-    color: "#60a5fa",
+    color: "hsl(var(--chart-2))",
+  },
+  receita: {
+    label: "Receita",
+    color: "hsl(var(--chart-3))",
   },
 } satisfies ChartConfig;
 
 export function Chart({ chartData }) {
+  const [activeChart, setActiveChart] =
+    useState<keyof typeof chartConfig>("pedidos");
+
   return (
-    <Card className="w-full">
-      <CardHeader>
-        <CardTitle>Dashboard</CardTitle>
-        <CardDescription>Últimos 30 dias</CardDescription>
+    <Card className="w-full max-w-full overflow-hidden">
+      <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+        <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-2 sm:py-2">
+          <CardTitle>Grafico Interativo</CardTitle>
+          <CardDescription>
+            Mostrando o total de {chartConfig[activeChart].label}
+          </CardDescription>
+        </div>
+        <div className="flex">
+          {["pedidos", "usuarios", "receita"].map((key) => {
+            const chart = key as keyof typeof chartConfig;
+            return (
+              <button
+                key={chart}
+                data-active={activeChart === chart}
+                className="relative z-30 flex flex-1 flex-col justify-center gap-1 border-t px-2 py-2 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-2 sm:py-2"
+                onClick={() => setActiveChart(chart)}
+              >
+                <span className="text-xs text-muted-foreground">
+                  {chartConfig[chart].label}
+                </span>
+              </button>
+            );
+          })}
+        </div>
       </CardHeader>
       <CardContent>
-        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+        <ChartContainer config={chartConfig} className="">
           <BarChart accessibilityLayer data={chartData}>
             <CartesianGrid vertical={false} />
             <XAxis
               dataKey="month"
               tickLine={false}
-              tickMargin={10}
               axisLine={false}
-              allowDataOverflow={true}
+              tickMargin={10}
               tickFormatter={(value) =>
                 new Date(value).toLocaleDateString("pt-BR", {
                   day: "numeric",
@@ -56,8 +84,11 @@ export function Chart({ chartData }) {
             />
             <ChartTooltip content={<ChartTooltipContent />} />
             <ChartLegend content={<ChartLegendContent />} />
-            <Bar dataKey="pedidos" fill="var(--color-pedidos)" radius={4} />
-            <Bar dataKey="usuarios" fill="var(--color-usuarios)" radius={4} />
+            <Bar
+              dataKey={activeChart}
+              fill={`var(--color-${activeChart})`}
+              radius={4}
+            />
           </BarChart>
         </ChartContainer>
       </CardContent>
--- src/app/(admin)/dashboard/_components/infos.tsx
+++ src/app/(admin)/dashboard/_components/infos.tsx
@@ -12,6 +12,7 @@ import { Label } from "@/components/ui/label";
 import InfoCard from "./cards";
 import { Chart } from "./chart";
 import useOrder from "@/hooks/useOrder";
+import { DatePickerWithRange } from "./date-picker";
 
 export default function InfoData() {
   const { order } = useOrder();
@@ -29,11 +30,12 @@ export default function InfoData() {
     }),
     pedidos: item.items.length,
     usuarios: order.map((item) => item.user.id).length,
+    receita: item.total.toFixed(2),
   }));
 
   return (
     <main className="flex flex-col gap-4 py-4">
-      <div className="flex flex-col md:flex-row gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <InfoCard
           label="Receita Total"
           icon={<DollarSign />}
@@ -51,26 +53,29 @@ export default function InfoData() {
           data={totalCoffees}
         />
       </div>
-      <div className="flex flex-col lg:flex-row gap-4 ">
-        <Chart chartData={chartData} />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="flex flex-col gap-2">
+          <DatePickerWithRange />
+          <Chart chartData={chartData} />
+        </div>
         <Card>
           <CardHeader>
             <CardTitle>Ultimas vendas</CardTitle>
             <CardDescription>
-              você fez {order.length} ultilmas vendas
+              Suas ultilmas {order.length} vendas
             </CardDescription>
           </CardHeader>
           <CardContent>
             {order.slice(0, 5).map((item) => (
               <div
                 key={item.id}
-                className="flex justify-between items-center px-2 w-[400px] transition-colors hover:bg-muted/50 rounded-md"
+                className="flex justify-between items-center px-2  transition-colors hover:bg-muted/50 rounded-md"
               >
                 <div>
                   <Label className="text-lg">{item.user.name}</Label>
                   <p className="text-gray-400">{item.user.email}</p>
                 </div>
-                <Label className="text-lg">R$ {item.total}</Label>
+                <Label className="text-base">R$ {item.total}</Label>
               </div>
             ))}
           </CardContent>
